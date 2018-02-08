8f982b50f18decca236673a1d746418de6401417
Alain Belair <belair.alain@gmail.com>
2018-02-08 10:58:36 -0500

    Remove button when in Profile page.

--- src/services/ActionService.php
+++ src/services/ActionService.php
@@ -41,17 +41,19 @@ class ActionService
         add_action('admin_bar_menu', function ($wp_admin_bar) use ($plugin) {
             //check if one of our page
             if ($page = @$_GET['page']){
-                if ($panel = $plugin->componentCtrl->searchComponentByType('panel', $page, 'slug')) {
-                    $button = new View(['View source', 'template' => new Template('<span class="ab-icon dashicon-before dashicons-media-code"></span><span class="ab-label">{$Content}</span>')]);
-                    $uses = str_replace('\\', '/', $panel['uses']);
-                    $href = str_replace('atkdemo', $plugin->getConfig('plugin/code_base_url'), $uses).'.php';
-                    $args = array(
-                        'id'    => 'atkdemo-code',
-                        'title' => $button->getHTML(),
-                        'href'  => $href,
-                        'meta'  => ['title' => 'View Source code on Github', 'target' => '_blank']
-                    );
-                    $wp_admin_bar->add_node( $args );
+                if ($page !== 'profile-index') {
+                    if ($panel = $plugin->componentCtrl->searchComponentByType('panel', $page, 'slug')) {
+                        $button = new View(['View source', 'template' => new Template('<span class="ab-icon dashicon-before dashicons-media-code"></span><span class="ab-label">{$Content}</span>')]);
+                        $uses = str_replace('\\', '/', $panel['uses']);
+                        $href = str_replace('atkdemo', $plugin->getConfig('plugin/code_base_url'), $uses).'.php';
+                        $args = array(
+                            'id'    => 'atkdemo-code',
+                            'title' => $button->getHTML(),
+                            'href'  => $href,
+                            'meta'  => ['title' => 'View Source code on Github', 'target' => '_blank']
+                        );
+                        $wp_admin_bar->add_node( $args );
+                    }
                 }
             }
         }, 999);
